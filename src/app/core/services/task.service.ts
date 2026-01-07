17d3eebca8bc7ae2dc0fac998b7c06e933bc9950
Suhhushi <jose.silvacosta718@gmail.com>
2026-01-07 11:12:37 +0100

    TP2

---
+++ src/app/core/services/task.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private tasks = [
+    { id: 1, title: 'Apprendre Angular' },
+    { id: 2, title: 'Créer TaskBoard Pro' }
+  ];
+
+  private tasksSubject = new BehaviorSubject(this.tasks);
+  tasks$ = this.tasksSubject.asObservable();
+
+  addTask(title: string) {
+    const newTask = { id: Date.now(), title };
+    this.tasks.push(newTask);
+    this.tasksSubject.next(this.tasks);
+  }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TaskService } from '../core/services/task.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  imports: [AsyncPipe],
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  private taskService = inject(TaskService);
+  tasks$ = this.taskService.tasks$;
+
+  addTask(title: string) {
+    if (title.trim()) {
+      this.taskService.addTask(title);
+    }
+  }
 }

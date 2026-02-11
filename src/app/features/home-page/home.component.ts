69b9a55cf70a9977896084477953b39d7215b492
Suhhushi <jose.silvacosta718@gmail.com>
2026-02-11 09:11:23 +0100

    page home et about

--- src/app/features/home-page/home.component.ts
+++ src/app/features/home-page/home.component.ts
@@ -1,11 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html'
+  imports: [RouterLink],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  
 }

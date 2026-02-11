7ca2aef9cb6adc61bef27922547bd0b2cab1c505
Suhhushi <jose.silvacosta718@gmail.com>
2026-02-11 08:47:49 +0100

    Auto-generated commit

---
+++ chunk-EZBHZRO7.js
@@ -0,0 +1,1 @@
+import{Da as o,Ua as n,Va as a,cb as p}from"./chunk-WARQSQ6L.js";var m=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-about"]],decls:2,vars:0,template:function(t,i){t&1&&(n(0,"p"),p(1,"about works!"),a())},encapsulation:2})};export{m as AboutComponent};
